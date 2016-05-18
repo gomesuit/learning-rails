e577f534e902e0bcab9c91ce88a7c462bc8de5b1
gomesuit <gomesuit@gmail.com>
2016-05-19 00:04:00 +0900

    practice scafold

--- railbook/db/schema.rb
+++ railbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517164852) do
+ActiveRecord::Schema.define(version: 20160518141242) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
