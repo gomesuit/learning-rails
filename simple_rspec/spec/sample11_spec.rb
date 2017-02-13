4aafa3b1391e49750353871b74ccc6a16cec7e25
gomesuit <gomesuit@gmail.com>
2017-02-13 07:30:16 +0000

    save

---
+++ simple_rspec/spec/sample11_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/user'
+
+RSpec.describe User do
+  describe '#greet' do
+    let(:user) { User.new(params) }
+    let(:params) { { name: 'たろう', age: age } }
+    context '12歳以下の場合' do
+      let(:age) { 12 }
+      it 'ひらがなで答えること' do
+        expect(user.greet).to eq 'ぼくはたろうだよ。'
+      end
+    end
+    context '13歳以上の場合' do
+      let(:age) { 13 }
+      it '漢字で答えること' do
+        expect(user.greet).to eq '僕はたろうです。'
+      end
+    end
+  end
+end
